fc57c4c919b6d95bec09f9b6efef06f66109549e
Iam-Bala1406 <balahussain1406@gmail.com>
2025-02-21 10:54:13 +0530

    Create SimpleWebServer.java

---
+++ SimpleWebServer.java
@@ -0,0 +1,26 @@
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class SimpleWebServer {
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/", new MyHandler());
+        server.setExecutor(null);
+        server.start();
+        System.out.println("Server started at http://<your-ec2-public-ip>:8080");
+    }
+
+    static class MyHandler implements HttpHandler {
+        public void handle(HttpExchange exchange) throws IOException {
+            String response = "<html><body><h1>Hello, World! Welcome to My Simple Java Web Server</h1></body></html>";
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+}
